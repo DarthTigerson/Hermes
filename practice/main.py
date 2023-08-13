586911e015eedb76a48b54430f50194ab170a478
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-08-13 20:34:58 +0200

    clean code push

---
+++ practice/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from routers import admin, contracts, country, currency, departments, employee, employers, employment, sites, status
+from database import engine
+import models
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(admin.router)
+app.include_router(contracts.router)
+app.include_router(country.router)
+app.include_router(currency.router)
+app.include_router(departments.router)
+app.include_router(employee.router)
+app.include_router(employers.router)
+app.include_router(employment.router)
+app.include_router(sites.router)
+app.include_router(status.router)

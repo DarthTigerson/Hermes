acd04a0019eb03522027fa3439853c94dde61118
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-08-13 21:51:47 +0200

    updated main to new routers

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from fastapi import FastAPI
-from routers import admin, contracts, country, currency, departments, employee, employers, employment, sites, status
+from routers import home, users, manage, admin
 from database import engine
 import models
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(home.router)
+app.include_router(users.router)
+app.include_router(manage.router)
 app.include_router(admin.router)
-app.include_router(contracts.router)
-app.include_router(country.router)
-app.include_router(currency.router)
-app.include_router(departments.router)
-app.include_router(employee.router)
-app.include_router(employers.router)
-app.include_router(employment.router)
-app.include_router(sites.router)
-app.include_router(status.router)

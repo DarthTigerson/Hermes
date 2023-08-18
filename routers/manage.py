8a112d3f370af90c5b1fbe33855c248625c6c08d
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-08-18 21:14:30 +0200

    manage will now load the the database tables

--- routers/manage.py
+++ routers/manage.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import Annotated
 from database import SessionLocal
 from pydantic import BaseModel, Field
-from models import Users, Roles
+from models import Departments, Sites, Contracts, Employers, Employment, Country, Currency
 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -25,5 +25,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.get("/")
-async def test(request: Request):
-    return templates.TemplateResponse("manage.html", {"request": request})
+async def test(request: Request, db: Session = Depends(get_db)):
+    departments = db.query(Departments).all()
+    sites = db.query(Sites).all()
+    contracts = db.query(Contracts).all()
+    employers = db.query(Employers).all()
+    employment = db.query(Employment).all()
+    country = db.query(Country).all()
+    currency = db.query(Currency).all()
+
+    return templates.TemplateResponse("manage.html", {"request": request, "departments": departments, "sites": sites, "contracts": contracts, "employers": employers, "employment": employment, "countries": country, "currencies": currency})

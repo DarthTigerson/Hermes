d8e372ec33b3c21ee0a63656078027dee303b798
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-08-19 21:47:44 +0200

    updated admin page to show dets from db

--- routers/admin.py
+++ routers/admin.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import Annotated
 from database import SessionLocal
 from pydantic import BaseModel, Field
-from models import Users, Roles
+from models import Users, Roles, Teams
 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -25,5 +25,9 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.get("/")
-async def test(request: Request):
-    return templates.TemplateResponse("admin.html", {"request": request})
+async def test(request: Request, db: Session = Depends(get_db)):
+    users = db.query(Users).order_by(Users.username).all()
+    roles = db.query(Roles).order_by(Roles.name).all()
+    teams = db.query(Teams).order_by(Teams.name).all()
+
+    return templates.TemplateResponse("admin.html", {"request": request, "users": users, "roles": roles, "teams": teams})

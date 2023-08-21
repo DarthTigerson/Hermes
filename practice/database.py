433b00970db40ebb545847850b20e2d8d489d4ce
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-08-21 17:31:11 +0200

    clean code push

--- practice/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./missestalos.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base = declarative_base()

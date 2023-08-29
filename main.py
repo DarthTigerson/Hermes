d3a4f752794d690d26e5c019e32deae5f3c83d64
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-08-29 23:04:22 +0200

    main fix

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from routers import employee, home, manage, admin, logging, about
 from database import engine
 import models
 from starlette.staticfiles import StaticFiles
-from starlette import status
-from starlette.responses import RedirectResponse
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/")
-async def root():
-    return RedirectResponse(url="/home", status_code=status.HTTP_302_FOUND)
 
 app.include_router(home.router)
 app.include_router(employee.router)

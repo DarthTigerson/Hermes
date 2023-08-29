50d457bc9abd624dda66ea969cf53595f00dc045
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-08-29 22:09:39 +0200

    added currencies startup

--- startup.py
+++ startup.py
@@ -15,7 +15,7 @@ def create_default_user():
         db.commit()
 
         # Create a default user
-        user = models.Users(first_name="Hermes", last_name="Admin", username="hermes", password=admin.get_password_hash('hermes'), role_id=role.id)
+        user = models.Users(first_name="Hermes", last_name="Admin", username="hermes", password=admin.get_password_hash('hermes'), role_id=role.id, team_id=0, active=True)
         db.add(user)
         db.commit()
 
@@ -25,4 +25,46 @@ def create_default_user():
     finally:
         db.close()
 
-create_default_user()
+def create_all_countries():
+    db = SessionLocal()
+    try:
+        # Delete all existing countries
+        db.query(models.Country).delete()
+
+        # Add all countries in the world
+        with open('static/data/countries.txt', 'r') as f:
+            for line in f:
+                name, short_name = line.strip().split(',')
+                country = models.Country(name=name, short_name=short_name)
+                db.add(country)
+        db.commit()
+
+        print("All countries added successfully.")
+    except Exception as e:
+        print(f"Error adding countries: {e}")
+    finally:
+        db.close()
+    
+def create_all_currencies():
+    db = SessionLocal()
+    try:
+        # Delete all existing currencies
+        db.query(models.Currency).delete()
+
+        # Add all currencies in the world
+        with open('static/data/currencies.txt', 'r') as f:
+            for line in f:
+                name, symbol = line.strip().split(',')
+                currencies = models.Currency(name=name, symbol=symbol)
+                db.add(currencies)
+        db.commit()
+
+        print("All currencies added successfully.")
+    except Exception as e:
+        print(f"Error adding currencies: {e}")
+    finally:
+        db.close()
+
+#create_default_user()
+#create_all_countries()
+create_all_currencies()

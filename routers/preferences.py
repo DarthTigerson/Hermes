775b5ca33b8988f8e60293863e26f1d620affc26
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-10-04 10:59:54 +0200

    first push

---
+++ routers/preferences.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, status, HTTPException, Request, Form
+from sqlalchemy.orm import Session
+from typing import Annotated
+from database import SessionLocal
+from pydantic import BaseModel, Field
+from routers.admin import get_current_user
+from routers.logging import create_log, Log
+import models, datetime
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from starlette import status
+from starlette.responses import RedirectResponse
+
+router = APIRouter(
+    prefix="/preferences",
+    tags=["preferences"],
+)
+
+templates = Jinja2Templates(directory='templates')
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+

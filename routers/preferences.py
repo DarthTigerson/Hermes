0f8b60554ec7aff043c02c97abd02966348ed8c5
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-10-04 18:37:12 +0200

    updates to support mail module

--- routers/preferences.py
+++ routers/preferences.py
@@ -47,7 +47,7 @@ async def get_preferences(request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse("preferences.html", {"request": request, "logged_in_user": user, "role_state": role_state, "preferences": preferences})
 
 @router.post("/", response_class=HTMLResponse)
-async def post_preferences(request: Request, db: Session = Depends(get_db), trigger_onboarded_employee: bool = Form(False), trigger_updated_employee: bool = Form(False), trigger_offboarded_employee: bool = Form(False), slack_webhook: str = Form(None), email_list: str = Form(None)):
+async def post_preferences(request: Request, db: Session = Depends(get_db), trigger_onboarded_employee: bool = Form(False), trigger_updated_employee: bool = Form(False), trigger_offboarded_employee: bool = Form(False), slack_webhook: str = Form(None), email_list: str = Form(None), email_smtp_server: str = Form(None), email_smtp_port: int = Form(587), email_smtp_username: str = Form(None), email_smtp_password: str = Form(None)):
     
     user = await get_current_user(request)
 
@@ -58,6 +58,8 @@ async def post_preferences(request: Request, db: Session = Depends(get_db), trig
 
     if role_state.preferences == False:
         return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+    
+    preferences = db.query(Preferences).order_by(Preferences.id.desc()).first()
 
     preferences_model = Preferences()
 
@@ -65,6 +67,13 @@ async def post_preferences(request: Request, db: Session = Depends(get_db), trig
     preferences_model.email_updated_employee = trigger_updated_employee
     preferences_model.email_offboarded_employee = trigger_offboarded_employee
     preferences_model.email_list = email_list
+    preferences_model.email_smtp_server = email_smtp_server
+    preferences_model.email_smtp_port = email_smtp_port
+    preferences_model.email_smtp_username = email_smtp_username
+    if email_smtp_server != None:
+        preferences_model.email_smtp_password = email_smtp_password
+    else:
+        preferences_model.email_smtp_password = preferences.email_smtp_password
     preferences_model.slack_webhook_channel = slack_webhook
     preferences_model.daily_user_reports = False
     preferences_model.monthly_user_reports = False

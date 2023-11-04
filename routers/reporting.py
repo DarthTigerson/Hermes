12f88f386a9b496a61e3f96ee6256e65e563e841
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-11-04 16:09:28 +0100

    reporting module first push

---
+++ routers/reporting.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends, status, HTTPException, Request, Form
+from sqlalchemy.orm import Session, aliased
+from typing import Annotated, Optional
+from database import SessionLocal
+from pydantic import BaseModel, Field
+from routers.admin import get_current_user
+from routers.logging import create_log, Log
+from routers.messaging import slack_send_message, email_send_message
+from datetime import datetime
+import models
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from starlette import status
+from starlette.responses import RedirectResponse
+
+router = APIRouter(
+    prefix="/reporting",
+    tags=["reporting"],
+)
+
+templates = Jinja2Templates(directory='templates')
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.get("/")
+async def get_reporting(request: Request, db: Session = Depends(get_db)):
+
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/admin/login", status_code=status.HTTP_302_FOUND)
+    
+    role_state = db.query(models.Roles).filter(models.Roles.id == user['role_id']).first()
+    
+    return templates.TemplateResponse("reporting.html", {"request": request, "user": user, "role_state": role_state, "nav": 'reporting'})

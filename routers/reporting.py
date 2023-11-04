b4bbcece6128f2acae321e9a9c14848f330c058d
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-11-04 19:49:51 +0100

    wip report modules update

--- routers/reporting.py
+++ routers/reporting.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field
 from routers.admin import get_current_user
 from routers.logging import create_log, Log
 from routers.messaging import slack_send_message, email_send_message
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import models
 
 from fastapi.responses import HTMLResponse
@@ -32,7 +32,18 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.get("/")
-async def get_reporting(request: Request, db: Session = Depends(get_db)):
+async def get_reporting(request: Request, report_type: Optional[int] = 0, start_date: Optional[datetime] = date.today() - timedelta(days=30), end_date: Optional[datetime] = date.today(), db: Session = Depends(get_db)):
+
+    header_value = {
+        "report_type": report_type,
+        "start_date": start_date,
+        "end_date": end_date
+    }
+
+    if report_type == 0:
+        report_data = None
+    else:
+        report_data = 'Data'
 
     user = await get_current_user(request)
     if user is None:
@@ -40,4 +51,4 @@ async def get_reporting(request: Request, db: Session = Depends(get_db)):
     
     role_state = db.query(models.Roles).filter(models.Roles.id == user['role_id']).first()
     
-    return templates.TemplateResponse("reporting.html", {"request": request, "logged_in_user": user, "role_state": role_state, "nav": 'reporting'})
+    return templates.TemplateResponse("reporting.html", {"request": request, "logged_in_user": user, "role_state": role_state, "nav": 'reporting', "header_value": header_value, "report_data": report_data})

a0a0dd1c273edc69d22b710bbae3377820a162df
Thomas Bonnici <thomas.bonnici@icloud.com>
2023-11-04 22:49:17 +0100

    wip download csv file

--- routers/reporting.py
+++ routers/reporting.py
@@ -79,3 +79,33 @@ async def get_reporting(request: Request, report_type: Optional[int] = 0, start_
     salary_pay_frequency = db.query(models.PayFrequency).order_by(models.PayFrequency.name).all()
     
     return templates.TemplateResponse("reporting.html", {"request": request, "logged_in_user": user, "role_state": role_state, "nav": 'reporting', "header_value": header_value, "report_data": report_data, "countries": countries, "sites": sites, "departments": departments, "currencies": currencies, "employment_contracts": employment_contracts, "employment_types": employment_types, "employers": employers, "hr_teams": hr_teams, "salary_pay_frequency": salary_pay_frequency})
+
+@router.get("/download_csv/{report_type}/{start_date}/{end_date}")
+async def download_csv(request: Request, report_type: int, start_date: datetime, end_date: datetime, db: Session = Depends(get_db)):
+
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/admin/login", status_code=status.HTTP_302_FOUND)
+
+    # Convert start_date and end_date to strings in the 'YYYY-MM-DD' format
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
+
+    if report_type == 1:
+        report_data = db.query(models.Employees)\
+            .filter(models.Employees.employment_status_id == 0)\
+            .all()
+
+        # Filter the data in Python
+        report_data = [employee for employee in report_data if start_date_str <= datetime.strptime(employee.start_date, '%Y-%m-%d').strftime('%Y-%m-%d') <= end_date_str]
+    elif report_type == 2:
+        report_data = db.query(models.Employees)\
+            .filter(models.Employees.employment_status_id == 1)\
+            .all()
+
+        # Filter the data in Python
+        report_data = [employee for employee in report_data if start_date_str <= datetime.strptime(employee.end_date, '%Y-%m-%d').strftime('%Y-%m-%d') <= end_date_str]
+    else:
+        report_data = None
+
+    
